c987363b10bb5f8c81aa39416fa4d1864093fcf2
IldarZar <63116956+IldarZar@users.noreply.github.com>
2022-11-30 19:16:26 +0500

    lesson 10

---
+++ tic-tac-toe.js
@@ -0,0 +1,30 @@
+import {TicTacToe} from "./components/TicTacToe.js";
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init)
+} else {
+    init()
+}
+
+function init() {    
+    const moveEl = document.getElementById('move-value')
+
+    const onMove = (isXTurn) => {
+        moveEl.innerText = isXTurn ? 'X' : '0'
+    }
+
+    const game = TicTacToe.init(
+        {
+            el: document.getElementById('tic-tac-toe'),
+            onMove,
+        }
+    )
+    
+    game.startGame()
+    
+    const restartBtn = document.getElementById('restart-btn')
+
+    restartBtn.addEventListener('click', () => {
+        game.restartGame()
+    })
+}
